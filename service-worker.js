fee025c9448a3d9fee070d98d3ab94e878ac16a1
Jay Meistrich <jmeistrich@gmail.com>
2018-03-03 12:02:05 +0800

    Release - 1520049235013

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-var CacheName = 'moodo-cache-1519986429894';
+var CacheName = 'moodo-cache-1520049235013',
+    CacheNameCommon = 'moodo-cache-common';
 
 self.addEventListener('install', function (e)
 {
@@ -8,43 +9,48 @@ self.addEventListener('install', function (e)
         {
             return cache.addAll([
                 '/app/',
-                '/app/index-1519986429894.html',
-                '/favicon.ico',
-                '/js/main-min-1519986429894.js',
-                '/js/preload-min-1519986429894.js',
-                '/js/rollbar-min.js',
-                '/css/app-min-1519986429894.css',
-                '/css/fonts/fonticons-1519986429894.woff',
-                '/css/fonts/fonticons-1519986429894.ttf',
-                '/img/stripe.png',
-                '/img/apple-touch-icon.png',
-                '/img/logoForWhite600.png',
-                '/img/btn_google_signin_dark_normal_web@2x.png',
-                '/img/btn_google_signin_dark_focus_web@2x.png',
-                '/img/btn_google_signin_dark_pressed_web@2x.png',
-                '/img/plugin-gdocs.png',
-                '/img/plugin-gsheets.png',
-                '/img/plugin-gslides.png',
-                '/img/plugin-image.png',
-                '/img/plugin-gcal.png',
-                '/img/plugin-mailbird.png',
-                '/img/plugin-gdrive.png',
-                '/img/plugin-gmail.png',
-                '/img/plugin-bear.png',
-                '/data/about.json',
-                '/data/beta-example.json',
-                '/data/blog-drive.json',
-                '/data/blog-proj-man.json',
-                '/data/blog-scrum.json',
-                '/data/blog-travel.json',
-                '/data/demo.json',
-                '/data/landing-email.json',
-                '/data/landing-old-intro.json',
-                '/data/landing-style.json',
-                '/data/landing-top.json',
-                '/data/press.json'
+                '/app/index-1520049235013.html',
+                '/js/main-min-1520049235013.js',
+                '/js/preload-min-1520049235013.js',
+                '/css/app-min-1520049235013.css',
+                '/css/fonts/fonticons-1520049235013.woff',
+                '/css/fonts/fonticons-1520049235013.ttf'
             ]);
-        })
+        }).then(caches.open(CacheNameCommon).then(function (cache)
+            {
+                return cache.addAll([
+                    '/favicon.ico',
+                    '/js/rollbar-min.js',
+                    '/img/stripe.png',
+                    '/img/apple-touch-icon.png',
+                    '/img/logoForWhite600.png',
+                    '/img/btn_google_signin_dark_normal_web@2x.png',
+                    '/img/btn_google_signin_dark_focus_web@2x.png',
+                    '/img/btn_google_signin_dark_pressed_web@2x.png',
+                    '/img/plugin-gdocs.png',
+                    '/img/plugin-gsheets.png',
+                    '/img/plugin-gslides.png',
+                    '/img/plugin-image.png',
+                    '/img/plugin-gcal.png',
+                    '/img/plugin-mailbird.png',
+                    '/img/plugin-gdrive.png',
+                    '/img/plugin-gmail.png',
+                    '/img/plugin-bear.png',
+                    '/data/about.json',
+                    '/data/beta-example.json',
+                    '/data/blog-drive.json',
+                    '/data/blog-proj-man.json',
+                    '/data/blog-scrum.json',
+                    '/data/blog-travel.json',
+                    '/data/demo.json',
+                    '/data/landing-email.json',
+                    '/data/landing-old-intro.json',
+                    '/data/landing-style.json',
+                    '/data/landing-top.json',
+                    '/data/press.json'
+                ]);
+            })
+        )
     );
 });
 
@@ -53,16 +59,16 @@ function endsWith(str, val)
     return str.indexOf(val, str.length - val.length) !== -1;
 }
 
-var baseUrl = 'moo.do/app/';
+var pathname = '/app/';
 
 self.addEventListener('fetch', function (event)
 {
+    const urlObj = new URL(event.request.url);
     var url = event.request.url;
 
-    if (url.endsWith(baseUrl) || url.indexOf(baseUrl + '#') > 0)
+    if (urlObj.origin === location.origin && urlObj.pathname === pathname)
     {
-        console.log('replacing');
-        url = url.replace(baseUrl, baseUrl + 'index-1519986429894.html');
+        url = url.replace(pathname, pathname + 'index-1520049235013.html');
     }
 
     event.respondWith(
@@ -76,9 +82,7 @@ self.addEventListener('fetch', function (event)
                 }
                 return fetch(event.request);
             }
-            ).catch(function (err) {
-                console.log(url, err);
-            })
+            ).catch(function () { })
     );
 });
 
@@ -90,11 +94,8 @@ self.addEventListener('activate', function (event)
             return Promise.all(
                 cacheNames.filter(function (cacheName)
                 {
-                    console.log('delete', cacheName, cacheName !== CacheName);
-                    return cacheName !== CacheName;
-                    // Return true if you want to remove this cache,
-                    // but remember that caches are shared across
-                    // the whole origin
+                    console.log('delete', cacheName, cacheName !== CacheName && cacheName !== CacheNameCommon);
+                    return cacheName !== CacheName && cacheName !== CacheNameCommon;
                 }).map(function (cacheName)
                 {
                     return caches.delete(cacheName);
@@ -102,4 +103,12 @@ self.addEventListener('activate', function (event)
             );
         })
     );
+
+    self.clients.matchAll({ includeUncontrolled: true }).then(function (all)
+    {
+        all.map(function (client)
+        {
+            client.postMessage('ServiceWorker_Activated');
+        });
+    });
 });
