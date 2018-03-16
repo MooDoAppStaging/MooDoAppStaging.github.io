ebaaa87a53b71469dfab0a5d7711711a0f66a147
Jay Meistrich <jmeistrich@gmail.com>
2018-03-17 01:11:19 +0900

    Release - 1521216380449

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var CacheName = 'moodo-cache-1521180738315',
+var CacheName = 'moodo-cache-1521216380449',
     CacheNameCommon = 'moodo-cache-common';
 
 function notifyClient(text)
@@ -22,12 +22,12 @@ self.addEventListener('install', function (e)
         {
             return cache.addAll([
                 '/app/',
-                '/app/index-1521180738315.html',
-                '/js/main-min-1521180738315.js',
-                '/js/preload-min-1521180738315.js',
-                '/css/app-min-1521180738315.css',
-                '/css/fonts/fonticons-1521180738315.woff',
-                '/css/fonts/fonticons-1521180738315.ttf'
+                '/app/index-1521216380449.html',
+                '/js/main-min-1521216380449.js',
+                '/js/preload-min-1521216380449.js',
+                '/css/app-min-1521216380449.css',
+                '/css/fonts/fonticons-1521216380449.woff',
+                '/css/fonts/fonticons-1521216380449.ttf'
             ]);
         }).then(caches.open(CacheNameCommon).then(function (cache)
         {
@@ -82,7 +82,7 @@ self.addEventListener('fetch', function (event)
     {
         if (urlObj.pathname === pathname)
         {
-            url = url.replace(pathname, pathname + 'index-1521180738315.html');
+            url = url.replace(pathname, pathname + 'index-1521216380449.html');
         }
 
         event.respondWith(
