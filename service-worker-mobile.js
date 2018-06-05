9629830024a17a8b259fc055d50199bbb5c8b5e8
Jay Meistrich <jmeistrich@gmail.com>
2018-06-06 01:11:43 +0700

    Release - 1528046928720

---
+++ service-worker-mobile.js
@@ -0,0 +1,117 @@
+var CacheName = 'moodo-cache-1528222033563',
+    CacheNameCommon = 'moodo-cache-common';
+
+function notifyClient(text)
+{
+    self.clients.matchAll({ includeUncontrolled: true }).then(function (all)
+    {
+        all.map(function (client)
+        {
+            client.postMessage('ServiceWorker_' + text);
+        });
+    });
+}
+
+self.addEventListener('install', function (e)
+{
+    console.log('Installing new Service Worker');
+
+    self.skipWaiting();
+    e.waitUntil(
+        caches.open(CacheName).then(function (cache)
+        {
+            return cache.addAll([
+                '/appmobile/',
+                '/appmobile/index-1528222033563.html',
+                '/appmobile/js/vendor-1528222033563.js',
+                '/appmobile/js/delayedUI-1528222033563.js',
+                '/appmobile/js/dimport-1528222033563.js',
+                '/appmobile/js/main-min-1528222033563.js',
+                '/appmobile/js/preload-min-1528222033563.js',
+                '/appmobile/css/app-min-1528222033563.css',
+                '/appmobile/css/fonts/fonticons-1528222033563.woff',
+                '/appmobile/css/fonts/fonticons-1528222033563.ttf'
+            ]);
+        }).then(caches.open(CacheNameCommon).then(function (cacheCommon)
+        {
+            return cacheCommon.addAll([
+                '/favicon.ico',
+                '/js/rollbar-min.js',
+                '/img/stripe.png',
+                '/img/apple-touch-icon.png',
+                '/img/logoForWhite600.png',
+                '/img/btn_google_signin_dark_normal_web@2x.png',
+                '/img/btn_google_signin_dark_focus_web@2x.png',
+                '/img/btn_google_signin_dark_pressed_web@2x.png',
+                '/img/plugin-gdocs.png',
+                '/img/plugin-gsheets.png',
+                '/img/plugin-gslides.png',
+                '/img/plugin-image.png',
+                '/img/plugin-gcal.png',
+                '/img/plugin-mailbird.png',
+                '/img/plugin-gdrive.png',
+                '/img/plugin-gmail.png',
+                '/img/plugin-bear.png'
+            ]);
+        })).then(function ()
+        {
+            notifyClient('Installed');
+
+            return self.skipWaiting();
+        })
+    );
+});
+
+var pathname = '/appmobile/';
+
+self.addEventListener('fetch', function (event)
+{
+    const urlObj = new URL(event.request.url);
+    var url = event.request.url;
+
+    if (urlObj.origin === location.origin)
+    {
+        if (urlObj.pathname === pathname)
+        {
+            url = url.replace(pathname, pathname + 'index-1528222033563.html');
+        }
+
+        event.respondWith(
+            caches.match(url).then(function (response)
+            {
+                return response || fetch(event.request).then(function (response2)
+                {
+                    var response3 = response2.clone();
+                    caches.open(CacheNameCommon).then(function (cache)
+                    {
+                        cache.put(event.request, response3.clone());
+                    })
+                    return response2;
+                });
+            })
+        );
+    }
+
+});
+
+self.addEventListener('activate', function (event)
+{
+    console.log('Activating new Service Worker');
+
+    event.waitUntil(
+        caches.keys().then(function (cacheNames)
+        {
+            return Promise.all(
+                cacheNames.filter(function (cacheName)
+                {
+                    return cacheName !== CacheName && cacheName !== CacheNameCommon;
+                }).map(function (cacheName)
+                {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+
+    notifyClient('Activated');
+});
