7e504ff11296125388cbc8afaf9ec7acc46a86d9
Jay Meistrich <jmeistrich@gmail.com>
2018-07-29 15:12:03 +0200

    Release - Web: 1532869729029

--- service-worker-web.js
+++ service-worker-web.js
@@ -1,4 +1,4 @@
-var CacheName = 'moodo-cache-1532614959934',
+var CacheName = 'moodo-cache-1532869729029',
     CacheNameCommon = 'moodo-cache-data';
 
 function notifyClient(text)
@@ -22,15 +22,15 @@ self.addEventListener('install', function (e)
         {
             return cache.addAll([
                 '/web/',
-                '/web/index-1532614959934.html',
-                '/web/js/vendor-1532614959934.js',
-                '/web/js/delayedUI-1532614959934.js',
-                '/web/js/dimport-1532614959934.js',
-                '/web/js/main-min-1532614959934.js',
-                '/web/js/preload-min-1532614959934.js',
-                '/web/css/app-min-1532614959934.css',
-                '/web/css/fonts/fonticons-1532614959934.woff',
-                '/web/css/fonts/fonticons-1532614959934.ttf'
+                '/web/index-1532869729029.html',
+                '/web/js/vendor-1532869729029.js',
+                '/web/js/delayedUI-1532869729029.js',
+                '/web/js/dimport-1532869729029.js',
+                '/web/js/main-min-1532869729029.js',
+                '/web/js/preload-min-1532869729029.js',
+                '/web/css/app-min-1532869729029.css',
+                '/web/css/fonts/fonticons-1532869729029.woff',
+                '/web/css/fonts/fonticons-1532869729029.ttf'
             ]);
         }).then(caches.open(CacheNameCommon).then(function (cacheCommon)
         {
@@ -73,7 +73,7 @@ self.addEventListener('fetch', function (event)
     {
         if (urlObj.pathname === pathname)
         {
-            url = url.replace(pathname, pathname + 'index-1532614959934.html');
+            url = url.replace(pathname, pathname + 'index-1532869729029.html');
         }
 
         event.respondWith(
