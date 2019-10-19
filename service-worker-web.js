5ce7c3fd032ea1dc7dd5410e048c695256e8e684
Jay Meistrich <jmeistrich@gmail.com>
2019-10-20 10:15:54 +1300

    Release - Web: 1571519684021

--- service-worker-web.js
+++ service-worker-web.js
@@ -1,4 +1,4 @@
-var CacheName = 'moodo-cache-1571441124803',
+var CacheName = 'moodo-cache-1571519684021',
     CacheNameCommon = 'moodo-cache-data';
 
 function notifyClient(text)
@@ -22,16 +22,16 @@ self.addEventListener('install', function (e)
         {
             return cache.addAll([
                 '/web/',
-                '/web/index-1571441124803.html',
-                '/web/js/vendor-1571441124803.js',
-                '/web/js/delayedUI-1571441124803.js',
-                '/web/js/dimport-1571441124803.js',
-                '/web/js/app-1571441124803.js',
-                '/web/js/preload-1571441124803.js',
-                '/web/js/preload.worker-1571441124803.js',
-                '/web/css/app-min-1571441124803.css',
-                '/web/css/fonts/fonticons-1571441124803.woff',
-                '/web/css/fonts/fonticons-1571441124803.ttf'
+                '/web/index-1571519684021.html',
+                '/web/js/vendor-1571519684021.js',
+                '/web/js/delayedUI-1571519684021.js',
+                '/web/js/dimport-1571519684021.js',
+                '/web/js/app-1571519684021.js',
+                '/web/js/preload-1571519684021.js',
+                '/web/js/preload.worker-1571519684021.js',
+                '/web/css/app-min-1571519684021.css',
+                '/web/css/fonts/fonticons-1571519684021.woff',
+                '/web/css/fonts/fonticons-1571519684021.ttf'
             ]);
         }).then(caches.open(CacheNameCommon).then(function (cacheCommon)
         {
@@ -74,7 +74,7 @@ self.addEventListener('fetch', function (event)
     {
         if (urlObj.pathname === pathname)
         {
-            url = url.replace(pathname, pathname + 'index-1571441124803.html');
+            url = url.replace(pathname, pathname + 'index-1571519684021.html');
         }
 
         event.respondWith(
