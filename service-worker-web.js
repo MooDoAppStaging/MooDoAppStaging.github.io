c43422e3fc715290673b0143b13b67eb1aedbd0b
Jay Meistrich <jmeistrich@gmail.com>
2020-09-11 20:38:02 +0800

    Release - Web: 1599827684250

--- service-worker-web.js
+++ service-worker-web.js
@@ -1,4 +1,4 @@
-var CacheName = 'moodo-cache-1599827162488',
+var CacheName = 'moodo-cache-1599827684250',
     CacheNameCommon = 'moodo-cache-data';
 
 function notifyClient(text)
@@ -22,19 +22,19 @@ self.addEventListener('install', function (e)
         {
             return cache.addAll([
                 '/web/',
-                '/web/index-1599827162488.html',
-                '/web/js/vendor-1599827162488.js',
-                '/web/js/delayedUI-1599827162488.js',
-                '/web/js/codeBlock-1599827162488.js',
-                '/web/js/dimport-1599827162488.js',
-                '/web/js/textEncoding-1599827162488.js',
-                '/web/js/app-1599827162488.js',
-                '/web/js/preload-1599827162488.js',
-                '/web/js/preload.worker-1599827162488.js',
-                '/web/css/app-min-1599827162488.css',
-                '/web/css/fonticons-1599827162488.css',
-                '/web/css/fonts/fonticons-1599827162488.woff',
-                '/web/css/fonts/fonticons-1599827162488.ttf'
+                '/web/index-1599827684250.html',
+                '/web/js/vendor-1599827684250.js',
+                '/web/js/delayedUI-1599827684250.js',
+                '/web/js/codeBlock-1599827684250.js',
+                '/web/js/dimport-1599827684250.js',
+                '/web/js/textEncoding-1599827684250.js',
+                '/web/js/app-1599827684250.js',
+                '/web/js/preload-1599827684250.js',
+                '/web/js/preload.worker-1599827684250.js',
+                '/web/css/app-min-1599827684250.css',
+                '/web/css/fonticons-1599827684250.css',
+                '/web/css/fonts/fonticons-1599827684250.woff',
+                '/web/css/fonts/fonticons-1599827684250.ttf'
             ]);
         }).then(caches.open(CacheNameCommon).then(function (cacheCommon)
         {
@@ -77,7 +77,7 @@ self.addEventListener('fetch', function (event)
     {
         if (urlObj.pathname === pathname)
         {
-            url = url.replace(pathname, pathname + 'index-1599827162488.html');
+            url = url.replace(pathname, pathname + 'index-1599827684250.html');
         }
 
         event.respondWith(
