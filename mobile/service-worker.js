f69b519cd6019d0c7cfe24f7f47ad354bd2f8ea1
Jay Meistrich <jmeistrich@gmail.com>
2020-10-04 17:02:07 +0800

    Release - Web: 1601801790483

--- mobile/service-worker.js
+++ mobile/service-worker.js
@@ -1,4 +1,4 @@
-var CacheName = 'moodo-cache-1601699367872',
+var CacheName = 'moodo-cache-1601802005072',
     CacheNameCommon = 'moodo-cache-data';
 
 function notifyClient(text)
@@ -22,20 +22,20 @@ self.addEventListener('install', function (e)
         {
             return cache.addAll([
                 '/mobile/',
-                '/mobile/index-1601699367872.html',
-                '/mobile/js/vendor-1601699367872.js',
-                // '/mobile/js/delayedUI-1601699367872.js',
-                '/mobile/js/codeBlock-1601699367872.js',
-                // '/mobile/js/dimport-1601699367872.js',
-                '/mobile/js/textEncoding-1601699367872.js',
-                // '/mobile/js/braintree-1601699367872.js',
-                '/mobile/js/app-1601699367872.js',
-                '/mobile/js/preload-1601699367872.js',
-                '/mobile/js/preload.worker-1601699367872.js',
-                '/mobile/css/app-min-1601699367872.css',
-                '/mobile/css/fonticons-1601699367872.css',
-                '/mobile/css/fonts/fonticons-1601699367872.woff',
-                '/mobile/css/fonts/fonticons-1601699367872.ttf'
+                '/mobile/index-1601802005072.html',
+                '/mobile/js/vendor-1601802005072.js',
+                // '/mobile/js/delayedUI-1601802005072.js',
+                '/mobile/js/codeBlock-1601802005072.js',
+                // '/mobile/js/dimport-1601802005072.js',
+                '/mobile/js/textEncoding-1601802005072.js',
+                // '/mobile/js/braintree-1601802005072.js',
+                '/mobile/js/app-1601802005072.js',
+                '/mobile/js/preload-1601802005072.js',
+                '/mobile/js/preload.worker-1601802005072.js',
+                '/mobile/css/app-min-1601802005072.css',
+                '/mobile/css/fonticons-1601802005072.css',
+                '/mobile/css/fonts/fonticons-1601802005072.woff',
+                '/mobile/css/fonts/fonticons-1601802005072.ttf'
             ]);
         }).then(caches.open(CacheNameCommon).then(function (cacheCommon)
         {
@@ -78,7 +78,7 @@ self.addEventListener('fetch', function (event)
     {
         if (urlObj.pathname === pathname)
         {
-            url = url.replace(pathname, pathname + 'index-1601699367872.html');
+            url = url.replace(pathname, pathname + 'index-1601802005072.html');
         }
 
         event.respondWith(
--- service-worker-web.js
+++ service-worker-web.js
@@ -1,4 +1,4 @@
-var CacheName = 'moodo-cache-1601702209903',
+var CacheName = 'moodo-cache-1601801790483',
     CacheNameCommon = 'moodo-cache-data';
 
 function notifyClient(text)
@@ -22,20 +22,20 @@ self.addEventListener('install', function (e)
         {
             return cache.addAll([
                 '/web/',
-                '/web/index-1601702209903.html',
-                '/web/js/vendor-1601702209903.js',
-                '/web/js/delayedUI-1601702209903.js',
-                '/web/js/codeBlock-1601702209903.js',
-                '/web/js/dimport-1601702209903.js',
-                '/web/js/textEncoding-1601702209903.js',
-                '/web/js/braintree-1601702209903.js',
-                '/web/js/app-1601702209903.js',
-                '/web/js/preload-1601702209903.js',
-                '/web/js/preload.worker-1601702209903.js',
-                '/web/css/app-min-1601702209903.css',
-                '/web/css/fonticons-1601702209903.css',
-                '/web/css/fonts/fonticons-1601702209903.woff',
-                '/web/css/fonts/fonticons-1601702209903.ttf'
+                '/web/index-1601801790483.html',
+                '/web/js/vendor-1601801790483.js',
+                '/web/js/delayedUI-1601801790483.js',
+                '/web/js/codeBlock-1601801790483.js',
+                '/web/js/dimport-1601801790483.js',
+                '/web/js/textEncoding-1601801790483.js',
+                '/web/js/braintree-1601801790483.js',
+                '/web/js/app-1601801790483.js',
+                '/web/js/preload-1601801790483.js',
+                '/web/js/preload.worker-1601801790483.js',
+                '/web/css/app-min-1601801790483.css',
+                '/web/css/fonticons-1601801790483.css',
+                '/web/css/fonts/fonticons-1601801790483.woff',
+                '/web/css/fonts/fonticons-1601801790483.ttf'
             ]);
         }).then(caches.open(CacheNameCommon).then(function (cacheCommon)
         {
@@ -78,7 +78,7 @@ self.addEventListener('fetch', function (event)
     {
         if (urlObj.pathname === pathname)
         {
-            url = url.replace(pathname, pathname + 'index-1601702209903.html');
+            url = url.replace(pathname, pathname + 'index-1601801790483.html');
         }
 
         event.respondWith(
