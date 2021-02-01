b39099edf912754156ea185e5441665b04b12904
Jay Meistrich <jmeistrich@gmail.com>
2021-02-01 20:36:13 +0800

    Release - Web: 1612182844810

--- web/service-worker.js
+++ web/service-worker.js
@@ -1,4 +1,4 @@
-var CacheName = 'moodo-cache-1612182579950';
+var CacheName = 'moodo-cache-1612182844810';
 var CacheNameCommon = 'moodo-cache-data';
 
 function notifyClient(text)
@@ -21,20 +21,20 @@ self.addEventListener('install', function (e)
         {
             return cache.addAll([
                 '/web/',
-                '/web/index-1612182579950.html',
-                '/web/js/vendor-1612182579950.js',
-                '/web/js/delayedUI-1612182579950.js',
-                '/web/js/codeBlock-1612182579950.js',
-                '/web/js/dimport-1612182579950.js',
-                '/web/js/textEncoding-1612182579950.js',
-                '/web/js/braintree-1612182579950.js',
-                '/web/js/app-1612182579950.js',
-                '/web/js/preload-1612182579950.js',
-                '/web/js/preload.worker-1612182579950.js',
-                '/web/css/app-min-1612182579950.css',
-                '/web/css/fonticons-1612182579950.css',
-                '/web/css/fonts/fonticons-1612182579950.woff',
-                '/web/css/fonts/fonticons-1612182579950.ttf'
+                '/web/index-1612182844810.html',
+                '/web/js/vendor-1612182844810.js',
+                '/web/js/delayedUI-1612182844810.js',
+                '/web/js/codeBlock-1612182844810.js',
+                '/web/js/dimport-1612182844810.js',
+                '/web/js/textEncoding-1612182844810.js',
+                '/web/js/braintree-1612182844810.js',
+                '/web/js/app-1612182844810.js',
+                '/web/js/preload-1612182844810.js',
+                '/web/js/preload.worker-1612182844810.js',
+                '/web/css/app-min-1612182844810.css',
+                '/web/css/fonticons-1612182844810.css',
+                '/web/css/fonts/fonticons-1612182844810.woff',
+                '/web/css/fonts/fonticons-1612182844810.ttf'
             ]);
         }).then(caches.open(CacheNameCommon).then(function (cacheCommon)
         {
@@ -96,7 +96,7 @@ self.addEventListener('fetch', function (event)
     {
         if (urlObj.pathname === pathname)
         {
-            url = url.replace(pathname, pathname + 'index-1612182579950.html');
+            url = url.replace(pathname, pathname + 'index-1612182844810.html');
         }
 
         event.respondWith(
