e6f671bb7092ca244094c3498db764175d4f1a6a
Jay Meistrich <jmeistrich@gmail.com>
2021-03-15 18:47:01 +0800

    Release - Web: 1615805061928

--- web/service-worker.js
+++ web/service-worker.js
@@ -1,4 +1,4 @@
-var CacheName = 'moodo-cache-1615796271539';
+var CacheName = 'moodo-cache-1615805061928';
 var CacheNameCommon = 'moodo-cache-data';
 
 function notifyClient(text)
@@ -21,20 +21,20 @@ self.addEventListener('install', function (e)
         {
             return cache.addAll([
                 '/web/',
-                '/web/index-1615796271539.html',
-                '/web/js/vendor-1615796271539.js',
-                '/web/js/delayedUI-1615796271539.js',
-                '/web/js/codeBlock-1615796271539.js',
-                '/web/js/dimport-1615796271539.js',
-                '/web/js/textEncoding-1615796271539.js',
-                '/web/js/braintree-1615796271539.js',
-                '/web/js/app-1615796271539.js',
-                '/web/js/preload-1615796271539.js',
-                '/web/js/preload.worker-1615796271539.js',
-                '/web/css/app-min-1615796271539.css',
-                '/web/css/fonticons-1615796271539.css',
-                '/web/css/fonts/fonticons-1615796271539.woff',
-                '/web/css/fonts/fonticons-1615796271539.ttf'
+                '/web/index-1615805061928.html',
+                '/web/js/vendor-1615805061928.js',
+                '/web/js/delayedUI-1615805061928.js',
+                '/web/js/codeBlock-1615805061928.js',
+                '/web/js/dimport-1615805061928.js',
+                '/web/js/textEncoding-1615805061928.js',
+                '/web/js/braintree-1615805061928.js',
+                '/web/js/app-1615805061928.js',
+                '/web/js/preload-1615805061928.js',
+                '/web/js/preload.worker-1615805061928.js',
+                '/web/css/app-min-1615805061928.css',
+                '/web/css/fonticons-1615805061928.css',
+                '/web/css/fonts/fonticons-1615805061928.woff',
+                '/web/css/fonts/fonticons-1615805061928.ttf'
             ]);
         }).then(caches.open(CacheNameCommon).then(function (cacheCommon)
         {
@@ -76,6 +76,7 @@ self.addEventListener('install', function (e)
             }
 
             notifyClient('Installed');
+            console.log('Service Worker: installed');
 
             if (!supportsWaiting)
             {
@@ -96,7 +97,7 @@ self.addEventListener('fetch', function (event)
     {
         if (urlObj.pathname === pathname)
         {
-            url = url.replace(pathname, pathname + 'index-1615796271539.html');
+            url = url.replace(pathname, pathname + 'index-1615805061928.html');
         }
 
         event.respondWith(
